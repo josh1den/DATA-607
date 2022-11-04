fb10df4bbc5ab4919a9b64ffe83f078bb6ea9b77
Josh Iden <iden.josh@gmail.com>
2022-11-04 14:58:44 -0400

    commit HW

--- HW/HW7/DATA607_HW7.R
+++ HW/HW7/DATA607_HW7.R
@@ -22,12 +22,19 @@ print(xml)
 ### READ JSON ###
 library(rjson)
 json_file <- "https://raw.githubusercontent.com/josh1den/DATA-607/main/HW/HW7/DATA607_HW7.json"
-json <- fromJSON(file=json) |>
+json <- fromJSON(file=json_file) |>
         as.data.frame()
-print(df_json)
+print(json)
 
 json_v2_file <- "https://raw.githubusercontent.com/josh1den/DATA-607/main/HW/HW7/DATA607_HW7_V2.json"
-json_v2 <- fromJSON(file=json_v2) |>
+json_v2 <- fromJSON(file=json_v2_file) |>
            as.data.frame()
 print(json_v2)
 
+library(arsenal)
+comparedf(xml, json_v2)
+
+xml == json_v2
+
+dplyr::all_equal(xml, json_v2)
+dplyr::all_equal(html, xml, json_v2)
